5933af46e9a01da8d08e595cb29c209e9bff42b2
jesusd0897 <jesusd0897@gmail.com>
2024-01-29 10:31:56 -0500

    Chore: Copyright updated
    - Code optimized and formatted

--- enzona-payment-embedded/src/main/java/com/github/jdsdhp/enzona/payment/embedded/data/datasource/remote/mapper/Mappers.kt
+++ enzona-payment-embedded/src/main/java/com/github/jdsdhp/enzona/payment/embedded/data/datasource/remote/mapper/Mappers.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 jesusd0897.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.mapper
 
 import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.request.AmountDto
@@ -5,8 +21,8 @@ import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.requ
 import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.request.ItemDto
 import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.response.ItemResponseDto
 import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.response.LinkResponseDto
-import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.response.cancel.CancelResponseDto
 import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.response.PaymentResponseDto
+import com.github.jdsdhp.enzona.payment.embedded.data.datasource.remote.dto.response.cancel.CancelResponseDto
 import com.github.jdsdhp.enzona.payment.embedded.domain.model.Amount
 import com.github.jdsdhp.enzona.payment.embedded.domain.model.CancelStatus
 import com.github.jdsdhp.enzona.payment.embedded.domain.model.Details
